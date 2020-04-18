f2bcd537efcb8b4c9af563dbbdff71ebba5cbefd
tkt <tktcorporation.go@gmail.com>
2020-04-19 04:31:31 +0900

    update pickup rotator

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
     }
 }
